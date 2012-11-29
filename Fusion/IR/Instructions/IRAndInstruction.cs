0d238542e410a89699027b1faab6a4ed3901bd39
Orvid King <blah38621@gmail.com>
2012-11-29 11:59:37 -0600

    Update Fusion/IR/Instructions/IRAndInstruction.cs

--- Fusion/IR/Instructions/IRAndInstruction.cs
+++ Fusion/IR/Instructions/IRAndInstruction.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 
 namespace Fusion.IR.Instructions
 {
-    public sealed class IRAndInstruction : IRInstruction
+    public class IRAndInstruction : IRInstruction
     {
         public IRAndInstruction() : base(IROpcode.And)
         {

d90801b1719b3cfff88b614544b3315359189419
Orvid King <blah38621@gmail.com>
2012-11-29 12:46:05 -0600

    Update Fusion/IR/Instructions/IRLoadObjectInstruction.cs

--- Fusion/IR/Instructions/IRLoadObjectInstruction.cs
+++ Fusion/IR/Instructions/IRLoadObjectInstruction.cs
@@ -1,17 +1,14 @@
-﻿using Fusion.CLI.Metadata;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using Fusion.CLI.Metadata;
 
 namespace Fusion.IR.Instructions
 {
-    public sealed class IRLoadObjectInstruction : IRInstruction
+    public class IRLoadObjectInstruction : IRInstruction
     {
-        public IRType Type = null;
+        public IRType Type { get; private set; }
 
-        public IRLoadObjectInstruction(IRType pType)
-            : base(IROpcode.LoadObject)
+        public IRLoadObjectInstruction(IRType pType) : base(IROpcode.LoadObject)
         {
             Type = pType;
         }

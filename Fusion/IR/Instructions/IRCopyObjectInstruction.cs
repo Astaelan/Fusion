8efece9a6359225144c75b1078ea64078707357e
Orvid King <blah38621@gmail.com>
2012-11-29 12:18:46 -0600

    Update Fusion/IR/Instructions/IRCopyObjectInstruction.cs

--- Fusion/IR/Instructions/IRCopyObjectInstruction.cs
+++ Fusion/IR/Instructions/IRCopyObjectInstruction.cs
@@ -1,17 +1,14 @@
-﻿using Fusion.CLI.Metadata;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using Fusion.CLI.Metadata;
 
 namespace Fusion.IR.Instructions
 {
-    public sealed class IRCopyObjectInstruction : IRInstruction
+    public class IRCopyObjectInstruction : IRInstruction
     {
-        public IRType Type = null;
+        public IRType Type { get; private set; }
 
-        public IRCopyObjectInstruction(IRType pType)
-            : base(IROpcode.CopyObject)
+        public IRCopyObjectInstruction(IRType pType) : base(IROpcode.CopyObject)
         {
             Type = pType;
         }

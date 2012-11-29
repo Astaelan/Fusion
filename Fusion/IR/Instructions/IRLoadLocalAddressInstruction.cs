2b91857d7f887d1f3665ff61112839937fdd6635
Orvid King <blah38621@gmail.com>
2012-11-29 12:39:08 -0600

    Update Fusion/IR/Instructions/IRLoadLocalAddressInstruction.cs

--- Fusion/IR/Instructions/IRLoadLocalAddressInstruction.cs
+++ Fusion/IR/Instructions/IRLoadLocalAddressInstruction.cs
@@ -1,16 +1,13 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 
 namespace Fusion.IR.Instructions
 {
-    public sealed class IRLoadLocalAddressInstruction : IRInstruction
+    public class IRLoadLocalAddressInstruction : IRInstruction
     {
-        public uint LocalIndex = 0;
+        public uint LocalIndex { get; private set; }
 
-        public IRLoadLocalAddressInstruction(uint pLocalIndex)
-            : base(IROpcode.LoadLocalAddress)
+        public IRLoadLocalAddressInstruction(uint pLocalIndex) : base(IROpcode.LoadLocalAddress)
         {
             LocalIndex = pLocalIndex;
         }

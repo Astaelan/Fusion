31a6c01fa0bd0e02b87e6428bfb20307a94a5e64
Orvid King <blah38621@gmail.com>
2012-11-29 12:34:38 -0600

    Update Fusion/IR/Instructions/IRLoadFieldInstruction.cs

--- Fusion/IR/Instructions/IRLoadFieldInstruction.cs
+++ Fusion/IR/Instructions/IRLoadFieldInstruction.cs
@@ -1,17 +1,14 @@
-﻿using Fusion.CLI.Metadata;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using Fusion.CLI.Metadata;
 
 namespace Fusion.IR.Instructions
 {
-    public sealed class IRLoadFieldInstruction : IRInstruction
+    public class IRLoadFieldInstruction : IRInstruction
     {
-        public IRField Field = null;
+        public IRField Field { get; private set; }
 
-        public IRLoadFieldInstruction(IRField pField)
-            : base(IROpcode.LoadField)
+        public IRLoadFieldInstruction(IRField pField) : base(IROpcode.LoadField)
         {
             Field = pField;
         }

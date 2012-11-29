38d5c3a9f04603bc7637a0dc31dc08ebef007e6c
Orvid King <blah38621@gmail.com>
2012-11-29 12:50:41 -0600

    Update Fusion/IR/Instructions/IRLoadReal64Instruction.cs

--- Fusion/IR/Instructions/IRLoadReal64Instruction.cs
+++ Fusion/IR/Instructions/IRLoadReal64Instruction.cs
@@ -1,16 +1,13 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 
 namespace Fusion.IR.Instructions
 {
-    public sealed class IRLoadReal64Instruction : IRInstruction
+    public class IRLoadReal64Instruction : IRInstruction
     {
-        public double Value = 0;
+        public double Value { get; private set; }
 
-        public IRLoadReal64Instruction(double pValue)
-            : base(IROpcode.LoadReal64)
+        public IRLoadReal64Instruction(double pValue) : base(IROpcode.LoadReal64)
         {
             Value = pValue;
         }

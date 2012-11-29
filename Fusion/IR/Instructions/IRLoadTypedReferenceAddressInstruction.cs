46a43db3dd3b68d471ef49eb2c376c58f569f959
Orvid King <blah38621@gmail.com>
2012-11-29 13:00:33 -0600

    Update Fusion/IR/Instructions/IRLoadTypedReferenceAddressInstruction.cs

--- Fusion/IR/Instructions/IRLoadTypedReferenceAddressInstruction.cs
+++ Fusion/IR/Instructions/IRLoadTypedReferenceAddressInstruction.cs
@@ -1,16 +1,13 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 
 namespace Fusion.IR.Instructions
 {
-    public sealed class IRLoadTypedReferenceAddressInstruction : IRInstruction
+    public class IRLoadTypedReferenceAddressInstruction : IRInstruction
     {
-        public IRType Type = null;
+        public IRType Type { get; private set; }
 
-        public IRLoadTypedReferenceAddressInstruction(IRType pType)
-            : base(IROpcode.LoadTypedReferenceAddress)
+        public IRLoadTypedReferenceAddressInstruction(IRType pType) : base(IROpcode.LoadTypedReferenceAddress)
         {
             Type = pType;
         }

a15fde81f67b0820e5a331abeda062a08148b676
Orvid King <blah38621@gmail.com>
2012-11-29 12:19:23 -0600

    Update Fusion/IR/Instructions/IRDuplicateInstruction.cs

--- Fusion/IR/Instructions/IRDuplicateInstruction.cs
+++ Fusion/IR/Instructions/IRDuplicateInstruction.cs
@@ -1,14 +1,11 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 
 namespace Fusion.IR.Instructions
 {
-    public sealed class IRDuplicateInstruction : IRInstruction
+    public class IRDuplicateInstruction : IRInstruction
     {
-        public IRDuplicateInstruction()
-            : base(IROpcode.Duplicate)
+        public IRDuplicateInstruction() : base(IROpcode.Duplicate)
         {
         }
     }

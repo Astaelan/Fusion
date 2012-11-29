7bdfc8adf0d121f6a76acdf0bfbf7a8b83df0613
Orvid King <blah38621@gmail.com>
2012-11-29 12:37:18 -0600

    Update Fusion/IR/Instructions/IRLoadIndirectInstruction.cs

--- Fusion/IR/Instructions/IRLoadIndirectInstruction.cs
+++ Fusion/IR/Instructions/IRLoadIndirectInstruction.cs
@@ -1,16 +1,13 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 
 namespace Fusion.IR.Instructions
 {
-    public sealed class IRLoadIndirectInstruction : IRInstruction
+    public class IRLoadIndirectInstruction : IRInstruction
     {
-        public IRType Type = null;
+        public IRType Type { get; private set; }
 
-        public IRLoadIndirectInstruction(IRType pType)
-            : base(IROpcode.LoadIndirect)
+        public IRLoadIndirectInstruction(IRType pType) : base(IROpcode.LoadIndirect)
         {
             Type = pType;
         }

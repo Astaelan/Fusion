eb3b4998140ae9d33909667c9c79bf22f285665b
Orvid King <blah38621@gmail.com>
2012-11-29 12:16:22 -0600

    Update Fusion/IR/Instructions/IRConvertCheckedInstruction.cs

--- Fusion/IR/Instructions/IRConvertCheckedInstruction.cs
+++ Fusion/IR/Instructions/IRConvertCheckedInstruction.cs
@@ -1,17 +1,20 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 
 namespace Fusion.IR.Instructions
 {
-    public sealed class IRConvertCheckedInstruction : IRInstruction
+    public class IRConvertCheckedInstruction : IRInstruction
     {
-        public IRType Type = null;
-        public IROverflowType OverflowType = IROverflowType.None;
+        public IRType Type { get; private set; }
+        
+        private IROverflowType mOverflowType = IROverflowType.None;
+        public IROverflowType OverflowType
+        {
+            get { return mOverflowType; }
+            private set { mOverflowType = value; }
+        }
 
-        public IRConvertCheckedInstruction(IRType pType, IROverflowType pOverflowType)
-            : base(IROpcode.ConvertChecked)
+        public IRConvertCheckedInstruction(IRType pType, IROverflowType pOverflowType) : base(IROpcode.ConvertChecked)
         {
             Type = pType;
             OverflowType = pOverflowType;

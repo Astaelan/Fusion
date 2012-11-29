616edde4b9c80f714670c6c98141edfbca4dbe16
Orvid King <blah38621@gmail.com>
2012-11-29 12:07:48 -0600

    Update Fusion/IR/Instructions/IRCallInstruction.cs

--- Fusion/IR/Instructions/IRCallInstruction.cs
+++ Fusion/IR/Instructions/IRCallInstruction.cs
@@ -6,19 +6,8 @@ namespace Fusion.IR.Instructions
 {
     public class IRCallInstruction : IRInstruction
     {
-        private IRMethod mTarget = null;
-        public IRMethod Target
-        {
-            get { return mTarget; }
-            private set { mTarget = value; }
-        }
-        
-        private bool mVirtual = false;
-        public bool Virtual
-        {
-            get { return mVirtual; }
-            protected set { mVirtual = value; }
-        }
+        public IRMethod Target { get; private set; }
+        public bool Virtual { get; protected set; }
 
         public IRCallInstruction(IRMethod pTarget, bool pVirtual) : base(IROpcode.Call)
         {

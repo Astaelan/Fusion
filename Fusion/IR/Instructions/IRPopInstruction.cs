64bce0ab1fb7ec0d3bc9f7e40b26e0d318179d5d
Orvid King <blah38621@gmail.com>
2012-11-30 07:45:10 -0600

    Update Fusion/IR/Instructions/IRPopInstruction.cs

--- Fusion/IR/Instructions/IRPopInstruction.cs
+++ Fusion/IR/Instructions/IRPopInstruction.cs
@@ -1,14 +1,11 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 
 namespace Fusion.IR.Instructions
 {
-    public sealed class IRPopInstruction : IRInstruction
+    public class IRPopInstruction : IRInstruction
     {
-        public IRPopInstruction()
-            : base(IROpcode.Pop)
+        public IRPopInstruction() : base(IROpcode.Pop)
         {
         }
     }

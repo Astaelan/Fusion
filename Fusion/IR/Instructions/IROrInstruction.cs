dac95248570d393fd8af10e1ca069f61eab92835
Orvid King <blah38621@gmail.com>
2012-11-30 07:44:39 -0600

    Update Fusion/IR/Instructions/IROrInstruction.cs

--- Fusion/IR/Instructions/IROrInstruction.cs
+++ Fusion/IR/Instructions/IROrInstruction.cs
@@ -1,14 +1,11 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 
 namespace Fusion.IR.Instructions
 {
-    public sealed class IROrInstruction : IRInstruction
+    public class IROrInstruction : IRInstruction
     {
-        public IROrInstruction()
-            : base(IROpcode.Or)
+        public IROrInstruction() : base(IROpcode.Or)
         {
         }
     }

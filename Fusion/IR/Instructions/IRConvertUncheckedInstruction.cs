83de449b110284157231a9b7e8faa59120887b21
Astaelan <astaelan@gmail.com>
2012-11-30 20:57:09 -0700

    Fixing previous commit

--- Fusion/IR/Instructions/IRConvertUncheckedInstruction.cs
+++ Fusion/IR/Instructions/IRConvertUncheckedInstruction.cs
@@ -3,13 +3,22 @@ using System.Collections.Generic;
 
 namespace Fusion.IR.Instructions
 {
-    public class IRConvertUncheckedInstruction : IRInstruction
+    public sealed class IRConvertUncheckedInstruction : IRInstruction
     {
         public IRType Type { get; private set; }
 
-        public IRConvertUncheckedInstruction(IRType pType) : base(IROpcode.ConvertUnchecked)
+        public IRConvertUncheckedInstruction(IRType pType) : base(IROpcode.ConvertUnchecked) { Type = pType; }
+
+        public override void Linearize(Stack<IRStackObject> pStack)
         {
-            Type = pType;
+            Sources.Add(new IRLinearizedTarget(pStack.Pop().LinearizedTarget));
+
+            IRStackObject result = new IRStackObject();
+            result.Type = Type;
+            result.LinearizedTarget = new IRLinearizedTarget(IRLinearizedTargetType.Local);
+            result.LinearizedTarget.LocalVariable.LocalVariableIndex = AddLinearizedLocal(Type);
+            Destination = new IRLinearizedTarget(result.LinearizedTarget);
+            pStack.Push(result);
         }
     }
 }
--- Fusion/IR/Instructions/IRDivideInstruction.cs
+++ Fusion/IR/Instructions/IRDivideInstruction.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 
 namespace Fusion.IR.Instructions
 {
-    public class IRDivideInstruction : IRInstruction
+    public sealed class IRDivideInstruction : IRInstruction
     {
         private IROverflowType mOverflowType = IROverflowType.None;
         public IROverflowType OverflowType
@@ -12,9 +12,23 @@ namespace Fusion.IR.Instructions
             private set { mOverflowType = value; }
         }
 
-        public IRDivideInstruction(IROverflowType pOverflowType) : base(IROpcode.Divide)
+        public IRDivideInstruction(IROverflowType pOverflowType) : base(IROpcode.Divide) { OverflowType = pOverflowType; }
+
+        public override void Linearize(Stack<IRStackObject> pStack)
         {
-            OverflowType = pOverflowType;
+            IRStackObject value2 = pStack.Pop();
+            IRStackObject value1 = pStack.Pop();
+
+            Sources.Add(new IRLinearizedTarget(value1.LinearizedTarget));
+            Sources.Add(new IRLinearizedTarget(value2.LinearizedTarget));
+
+            IRType resultType = Method.Assembly.AppDomain.BinaryNumericResult(value1.Type, value2.Type);
+            IRStackObject result = new IRStackObject();
+            result.Type = resultType;
+            result.LinearizedTarget = new IRLinearizedTarget(IRLinearizedTargetType.Local);
+            result.LinearizedTarget.LocalVariable.LocalVariableIndex = AddLinearizedLocal(resultType);
+            Destination = new IRLinearizedTarget(result.LinearizedTarget);
+            pStack.Push(result);
         }
     }
 }
